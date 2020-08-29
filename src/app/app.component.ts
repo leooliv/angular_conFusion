308fb011523c6dfb10b5c5dffdcd2641568c32ba
Leonardo de Oliveira <leonardo.dolivier@gmail.com>
2020-08-29 12:18:29 -0300

    Angular Services First Appearance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conFusion';
+
+  isShow = false;
+
+  toggleDisplay() {
+    this.isShow = !this.isShow;
+  }
 }
